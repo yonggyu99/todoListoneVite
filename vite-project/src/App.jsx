921d7600a2c88eb91c13cd8bd91890b99c604bdb
박용규 <yoyo5411@naver.com>
2025-12-11 18:44:31 +0900

    feat : mockData 추가

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,8 +2,32 @@ import "./App.css";
 import Header from "./components/header";
 import Editor from "./components/editor";
 import List from "./components/List";
+import { useState } from "react";
+
+const mockData = [
+  {
+    id: 0,
+    isDone: false,
+    content: "React Study",
+    date: new Date().getTime(),
+  },
+  {
+    id: 1,
+    isDone: false,
+    content: "React Study1",
+    date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    isDone: false,
+    content: "React Study2",
+    date: new Date().getTime(),
+  },
+];
 
 function App() {
+  const [todos, setTodos] = useState(mockData);
+
   return (
     <div className="App">
       <Header />
