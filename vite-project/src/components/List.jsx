03a8a4b343b2067933e60acde2e2084de5c37b74
박용규 <yoyo5411@naver.com>
2025-12-15 21:04:46 +0900

    feat : Search 기능 및 Id, List Rendering 구현

--- vite-project/src/components/List.jsx
+++ vite-project/src/components/List.jsx
@@ -1,16 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import "./List.css";
 import TodoItem from "./TodoItem";
 
-const List = () => {
+const List = ({ todos }) => {
+  const [search, setSearch] = useState("");
+
+  const onChangeSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const getFilteredData = () => {
+    if (search === "") {
+      return todos;
+    }
+    return todos.filter((todo) =>
+      todo.content.toLowerCase().includes(search.toLowerCase())
+    );
+  };
+
+  const filteredTodos = getFilteredData();
+
   return (
     <div className="List">
       <h4>Todo List</h4>
-      <input placeholder="검색어를 입력하세요" />
+      <input
+        value={search}
+        onChange={onChangeSearch}
+        placeholder="검색어를 입력하세요"
+      />
       <div className="toDosWrapper">
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
+        {filteredTodos.map((todo) => {
+          return <TodoItem key={todo.id} {...todo} />;
+        })}
       </div>
     </div>
   );
