9bfa68505820c2268b20e3669909085f09ccea29
박용규 <yoyo5411@naver.com>
2025-12-15 17:56:59 +0900

    feat : OnCreate  Enter, 공백 입력 X, 입력 시 초기화 기능 추가

--- vite-project/src/components/Editor.jsx
+++ vite-project/src/components/Editor.jsx
@@ -1,22 +1,36 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import "./Editor.css";
 
 const Editor = ({ onCreate }) => {
   const [content, setContent] = useState("");
+  const contentRef = useRef();
 
   const onChangeContent = (e) => {
     setContent(e.target.value);
   };
 
+  const onKeyDown = (e) => {
+    if (e.keyCode === 13) {
+      onSubmit();
+    }
+  };
+
   const onSubmit = () => {
+    if (content === "") {
+      contentRef.current.focus();
+      return;
+    }
     onCreate(content);
+    setContent("");
   };
 
   return (
     <div className="Editor">
       <input
+        ref={contentRef}
         value={content}
         onChange={onChangeContent}
+        onKeyDown={onKeyDown}
         placeholder="새로운 todo..."
       />
 
